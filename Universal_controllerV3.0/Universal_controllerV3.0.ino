d5ed5fb62af81b2f524fdedca9dd153133b8f4b8
liujiahui <liujiahiu@qq.com>
2019-09-23 23:44:27 +0800

    通用控制器V3.0的初始代码

---
+++ Universal_controllerV3.0/Universal_controllerV3.0.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Universal_controllerV3.0.ino
+    Created:	2019/9/23 23:40:40
+    Author:     DESKTOP-7FPETTL\Administrator
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
